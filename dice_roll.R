44e21511acc296683d02e097089f9758351be41f
RKOpTris <mukappa@Mus-MacBook-Pro.local>
2023-07-20 14:58:24 +0100

    updated dice_roll

--- dice_roll.R
+++ dice_roll.R
@@ -1,8 +1,14 @@
-dice_roll <- function(x){  # takes a vector of integers
+dice_roll <- function(x, sum = F){  # takes a vector of integers
   stopifnot(is.numeric(x), x %in% c(4, 6, 8, 10, 12, 20))
-  data.frame(die = sapply(x, function(x){paste0(x, "-sided")}),
-             result = sapply(x, sample, 1))
+  result <- data.frame(die = sapply(x, function(x){paste0(x, "-sided")}),
+                       result = sapply(x, sample, 1))
+  if(!sum){
+    result
+  } else {
+    print(paste(result$result, collapse = " + "))
+    sum(result$result)
+  }
 }
 
 dice_roll(6)
-dice_roll(c(4, 8, 20, 20))
+dice_roll(c(4, 8, 20, 20), sum = T)
